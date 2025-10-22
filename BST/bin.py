e9d76b437fecf68c8f88cda319e7fb0a51249f87
sridharanitharan <username420sri@gmail.com>
2025-10-22 11:41:15 +0530

    bst searching Tree

--- BST/bin.py
+++ BST/bin.py
@@ -23,20 +23,66 @@ class bin:
                 current.left = node(value)
             else:
                 self.__insert__(current.left,value)
+
+    def __postorder__(self,current):
+        if current:
+            self.__postorder__(current.left)
+            self.__postorder__(current.right)
+            print(current.value,end=' ')
+
+    def __inOrder__(self,current):
+        if current:
+            self.__inOrder__(current.left)
+            print(current.value,end=' ')
+            self.__inOrder__(current.right)
+    def __preorder__(self,current):
+        if current:
+            print(current.value,end=' ')
+            self.__preorder__(current.left)
+            self.__preorder__(current.right)
+
+    def __findone__(self,target,root):
+        if root is None:
+            return None 
+        if root.value == target:
+            return True 
+        elif root.value < target:
+            self.__findone__(target ,root.right)
+        else:
+            self.__findone__(target,root.left)
         
 
-    def display(self):
-        self._display_recursive(self.root)
+        return False
+    def search(self,target):
+        result = self.__findone__(target,self.root)
+        if result:
+            print("Target founded on the BinaryTres")
+        else:
+            print("Target Not found in this BinaryTree")
 
-    def _display_recursive(self, current):
-        if current:
-            self._display_recursive(current.left)
-            print(current.value, end=' ')
-            self._display_recursive(current.right)
+    def iterate(self,order=None):
+        if order is None:
+            print('Pre-Order')
+            self.__preorder__(self.root)
+            print('\n')
+        elif order  == True:
+            print('Post-Order')
+            self.__postorder__(self.root)
+            print('\n')
+        else:
+            print('In-Order')
+            self.__inOrder__(self.root)
+            print('\n')
 data = bin()
 data.insert(10)
+data.insert(9)
+data.insert(5)
 data.insert(4)
+data.insert(3)
+data.insert(7)
 data.insert(342)
 data.insert(34)
-data.insert(2)
-data.display()
+data.iterate()
+data.iterate(False)
+data.iterate(True)
+data.search(443)
